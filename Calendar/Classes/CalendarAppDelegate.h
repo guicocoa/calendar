e50e1a750b681d38b2673f9c0e02d19be910c673
Caleb Davenport <caleb@guicocoa.com>
2010-01-24 11:50:57 -0500

    added xcode project with calendar source

---
+++ Calendar/Classes/CalendarAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  CalendarAppDelegate.h
+//  Calendar
+//
+//  Created by Caleb Davenport on 1/23/10.
+//  Copyright GUI Cocoa Software 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+	UITabBarController *tabController;
+}
+
+@end
+
